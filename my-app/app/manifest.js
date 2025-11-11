321e999da83de80e7bc67c76f80e7864acc7ae6e
Leonardo Padilha <leopadilha677@gmail.com>
2025-11-11 11:07:28 -0300

    Adição de icones para natividade do PWA

--- my-app/app/manifest.js
+++ my-app/app/manifest.js
@@ -9,13 +9,53 @@ export default function manifest() {
     theme_color: '#d95f43', // Cor principal
     icons: [
       {
-        src: '/icons/icon-192x192.png',
+        src: '/icons/android/android-launchericon-48-48.png',
+        sizes: '48x48',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/android/android-launchericon-72-72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/android/android-launchericon-96-96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/android/android-launchericon-144-144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/android/android-launchericon-192-192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icons/icon-512x512.png',
+        src: '/icons/ios/180.png', // iPhone retina
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/ios/192.png', // Para compatibilidade geral
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/windows11/Square150x150Logo.scale-200.png', // Para Windows
+        sizes: '300x300',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/android/android-launchericon-512-512.png',
         sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/ios/1024.png', // Ícone de alta resolução para iOS e outros usos
+        sizes: '1024x1024',
         type: 'image/png',
       },
     ],
